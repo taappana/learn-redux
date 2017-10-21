2f7c2bf829d3150d0e9e28739340cb159e4e5bb9
taappana <herranen.jani@gmail.com>
2017-10-21 18:43:48 +0300

    Updating state and displaying likes increment on the page

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,6 +1,17 @@
 function posts(state = [], action) {
-  console.log(state, action);
-  return state;
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+    console.log('Incrementing likes!!')
+    const i = action.index;
+    return [
+      ...state.slice(0,i), // before the one we're updating
+      {...state[i], likes: state[i].likes + 1},
+      ...state.slice(i + 1), // after the one we're updating
+    ]
+    // return the updated state
+    default:
+      return state;
+  }
 }
 
 export default posts;
