be00d7ecf56224ba5f04145bc161401f14f6d62b
taappana <herranen.jani@gmail.com>
2017-10-21 18:59:28 +0300

    Single photo component now being displayed

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
     return (
       <div className="single-photo">
-        I'm the Single
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
       </div>
     )
   }
-})
+});
 
 export default Single;
